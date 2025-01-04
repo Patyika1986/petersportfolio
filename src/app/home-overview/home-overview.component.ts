23cce87852fd5a5601631c439b58a07d207e2b2e
Peter <peter.lakatos@outlook.de>
2025-01-04 08:58:03 +0100

    some design

--- src/app/home-overview/home-overview.component.ts
+++ src/app/home-overview/home-overview.component.ts
@@ -1,16 +1,42 @@
 import { Component } from '@angular/core';
-import { NavComponent } from "../components/nav/nav.component";
+import { NavComponent } from '../components/nav/nav.component';
 import { I18nService } from '../i18n.service';
 import { HttpClient } from '@angular/common/http';
 import { TranslateModule } from '@ngx-translate/core';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-home-overview',
-  imports: [NavComponent,TranslateModule],
+  imports: [NavComponent, TranslateModule, NgFor],
   templateUrl: './home-overview.component.html',
-  styleUrl: './home-overview.component.scss'
+  styleUrl: './home-overview.component.scss',
 })
 export class HomeOverviewComponent {
   constructor(public i18nService: I18nService) {}
 
-
+  icons = [
+    'HTML5',
+    'CSS3',
+    'JavaScript ',
+    'TypeScript ',
+    'Angular ',
+    'Git ',
+    'Node.js ',
+    'Express ',
+    'Bootstrap ',
+    'Materialize ',
+    'React ',
+    'Vue.js ',
+    'NPM ',
+    'Jasmine ',
+    'Karma ',
+    'Vercel',
+    'MySQL',
+    'Firebase',
+    'Firestore',
+    'Cloud Firestore',
+    'Realtime Database',
+    'Cloud Storage',
+    'Cloud Functions',
+    'Authentication',
+  ];
 }
