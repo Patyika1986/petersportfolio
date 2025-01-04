e1a43b67be937399e5e2fefc7c19ac6b65e6ee01
Peter <peter.lakatos@outlook.de>
2025-01-04 09:07:12 +0100

    sichern

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
     { path: 'home', loadChildren: () => import('./home-overview/home-overview.module').then(m => m.HomeOverviewModule) },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
