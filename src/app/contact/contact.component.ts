02b7a9df3196371c84195b0097599dd125a51923
Peter <peter.lakatos@outlook.de>
2025-01-06 15:06:11 +0100

    emailjs

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { environment } from '../../environments/environment.development';
+import * as emailjs from '@emailjs/browser';
+import { EmailJSResponseStatus } from '@emailjs/browser';
 @Component({
   selector: 'app-contact',
-  imports: [TranslateModule],
+  imports: [TranslateModule,FormsModule,ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      const { name, email, message } = this.contactForm.value;
+      const templateParams = {
+        from_name: name,
+        email_id: email,
+        message: message
+      };
+      
+      emailjs
+        .send(
+          environment.serviceId,
+          environment.templateId,
+          templateParams,
+          environment.publicKey
+        )
+        .then((response: EmailJSResponseStatus) => {
+          this.contactForm.reset();
+        })
+        .catch((error) => {
+          console.error('Error sending email:', error);
+        });
+    }
+  }
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 const routes: Routes = [
   { path: '', component: ContactComponent }
 ]
@@ -11,7 +11,9 @@ const routes: Routes = [
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class ContactModule { }
